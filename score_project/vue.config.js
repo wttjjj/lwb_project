3e8e3a03f831f21921dd008f9dfddabc0f6e694c
王婷婷 <wangtingting@192.168.0.5>
2025-04-16 00:09:16 +0800

    部署

--- score_project/vue.config.js
+++
@@ -1,7 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  outputDir: 'docs',
-  publicPath: process.env.NODE_ENV === 'production'
-        ? '/wtt_project/'
-        : '/',
-})
--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ module.exports = defineConfig( {
     // transpileDependencies: true,
     outputDir: 'docs',
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/wtt_project/'
+        ? '/lwb_project/'
         : '/',
 } );
